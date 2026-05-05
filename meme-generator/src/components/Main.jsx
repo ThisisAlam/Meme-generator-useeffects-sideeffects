d5b80ac2940c9117245e3c98b204185147e19b39
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-05-05 09:54:34 +0500

    updated the codebase

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,6 +1,13 @@
 import "../UI/main.css"
-
+import React from "react"
 export default function Main() {
+
+    const [meme, setMeme] = React.useState({
+        imageUrl: "http://i.imgflip.com/1bij.jpg",
+        topText : "One does not simply",
+        bottomText: "Walk into Mordor"
+    })
+    console.log(meme)
     return (
         <main className="main">
             <div className="form">
@@ -22,9 +29,9 @@ export default function Main() {
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
